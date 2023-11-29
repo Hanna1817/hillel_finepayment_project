9488009fc752ed7eaa436e8771dc1fb50108ab38
Hanna1817 <93122335+Hanna1817@users.noreply.github.com>
2023-11-30 00:40:14 +0300

    fix fineList.js

--- js/fineList.js
+++ js/fineList.js
@@ -9,13 +9,24 @@ console.log(DB)
 
 function searchFines(searchKey){
     var filteredArr = [];
-     DB.forEach(function(item) {
-        if (searchKey === item.номер | searchKey === item.тип) {
-            filteredArr.push(item);
-          
-        }
-      });
-    return filteredArr;
+  if (!isNaN(searchKey)) {
+    let number = parseInt(searchKey, 10);
+    let formatNumber = number.toString().padStart(3, "0");
+    
+    DB.forEach(function (item) {
+      if (formatNumber === item.номер) {
+        filteredArr.push(item);
+      }
+    });
+  } else {
+    DB.forEach(function (item) {
+      if (searchKey === item.тип) {
+        filteredArr.push(item);
+      }
+    });
+  }
+
+  return filteredArr;
 
    /* return [
         {номер: '001', тип: 'Перевищення швидкості', сума: 100, дата: '2023-01-15'}
